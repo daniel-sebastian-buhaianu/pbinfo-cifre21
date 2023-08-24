f06ebd19d608639627f36316f6c3b53e3b34d5a1
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-08-24 04:19:23 +0300

    sol 1

--- cifre21.c
+++ cifre21.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+	unsigned short n, i, count = 0;
+	unsigned x;
+	char found;
+
+	scanf("%hu", &n);
+
+	for (i = 0; i < n; i++) {
+		scanf("%u", &x);
+		
+		found = 0;
+		while (x && !found) {
+			if (x % 10 == 0) found = 1;
+
+			x /= 10;
+		}
+
+		if (found) count++;
+	}
+
+	printf("%hu", count);
+
+	return 0;
+}
+// scor 80/100
